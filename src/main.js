a8e8dd9b448b5b3c8b7389edca90c25f36b7b7b7
Shijie Zhou <cs5129606@gmail.com>
2020-01-09 00:34:26 -0700

    menu is done

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// vue navigation
 import VueNavigationBar from "vue-navigation-bar"
+import "vue-navigation-bar/dist/vue-navigation-bar.css";
 
 Vue.component("vue-navigation-bar", VueNavigationBar)
 Vue.config.productionTip = false
