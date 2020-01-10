85c70d342013042d9ea323d54efe378480f4fc02
Shijie Zhou <cs5129606@gmail.com>
2020-01-09 22:14:16 -0700

    finish route setitng

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home"><HelloWorld msg="Welcome to Your Vue.js App" /></div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'home',
+    components: {
+        HelloWorld
+    }
+};
 </script>
+
+<style lang="scss">
+.home {
+     font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+}
+</style>
--- src/views/Project.vue
+++ src/views/Project.vue
@@ -1,7 +1,48 @@
 <template>
-    <div><h1>Project:</h1></div>
+    <div class="project">
+        <h1>Project:</h1>
+        <img src="https://janikvonrotz.ch/images/Surveillance%20Berlin/1_Banner.jpg" alt="test" />
+    </div>
 </template>
 
 <script></script>
 
-<style></style>
+<style lang="scss">
+.project {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+   
+    // Small devices (landscape phones, 576px and up)
+    @media (min-width: 576px) {
+        & img {
+            width: 100%;
+        }
+    }
+
+    // Medium devices (tablets, 768px and up)
+    @media (min-width: 768px) {
+        & img {
+            max-width: 768px;
+        }
+    }
+    & img {
+        max-width: 768px;
+    }
+    // Large devices (desktops, 992px and up)
+    @media (min-width: 992px) {
+        & img {
+            max-width: 768px;
+        }
+    }
+
+    // Extra large devices (large desktops, 1200px and up)
+    @media (min-width: 1200px) {
+        & img {
+            max-width: 768px;
+        }
+    }
+}
+</style>
