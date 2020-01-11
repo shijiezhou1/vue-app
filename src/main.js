1ec6bb04710a04489162071658ab771a6cd89903
Shijie Zhou <cs5129606@gmail.com>
2020-01-10 23:21:57 -0700

    update information

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 // vue navigation
 import VueNavigationBar from "vue-navigation-bar"
 import "vue-navigation-bar/dist/vue-navigation-bar.css";
+import store from './store'
 
 Vue.component("vue-navigation-bar", VueNavigationBar)
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
