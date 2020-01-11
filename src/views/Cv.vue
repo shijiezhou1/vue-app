b32eb4640ccc101a3df50aeb3ec4d087666e39ce
Shijie Zhou <cs5129606@gmail.com>
2020-01-10 22:48:27 -0700

    used pdfvuer for now

--- src/views/Cv.vue
+++ src/views/Cv.vue
@@ -1,17 +1,71 @@
 <template>
-    <div>
-        <pdf src="./pdf/resume.pdf" :page="page" ref="myPdfComponent"></pdf>
+    <div id="pdfvuer">
+        <pdf :src="pdfdata" v-for="i in numPages" :key="i" :id="i" :page="i" :scale.sync="scale" style="width:100%;margin:20px auto;">
+            <template slot="loading">
+                loading content here...
+            </template>
+        </pdf>
     </div>
 </template>
 
 <script>
-import pdf from 'vue-pdf';
+// script is reference to advance pdfvuer
+import pdfvuer from 'pdfvuer';
 
 export default {
     components: {
-        pdf
+        pdf: pdfvuer
+    },
+    data() {
+        return {
+            page: 1,
+            numPages: 0,
+            pdfdata: undefined,
+            errors: [],
+            scale: 'page-width'
+        };
+    },
+    computed: {
+        formattedZoom() {
+            return Number.parseInt(this.scale * 100);
+        }
+    },
+    mounted() {
+        this.getPdf();
+    },
+    watch: {
+        show: function(s) {
+            if (s) {
+                this.getPdf();
+            }
+        },
+        page: function(p) {
+            if (
+                window.pageYOffset <= this.findPos(document.getElementById(p)) ||
+                (document.getElementById(p + 1) && window.pageYOffset >= this.findPos(document.getElementById(p + 1)))
+            ) {
+                // window.scrollTo(0,this.findPos(document.getElementById(p)));
+                document.getElementById(p).scrollIntoView();
+            }
+        }
+    },
+    methods: {
+        getPdf() {
+            var self = this;
+            self.pdfdata = pdfvuer.createLoadingTask('./pdf/resume.pdf');
+            self.pdfdata.then(pdf => {
+                self.numPages = pdf.numPages;
+            });
+        },
+        findPos(obj) {
+            return obj.offsetTop;
+        }
     }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.content {
+    padding: 16px;
+}
+</style>
