e69da3e09ae01e2565433082c0489841387d36e1
Shijie Zhou <cs5129606@gmail.com>
2020-02-09 19:38:42 -0700

    adding information about style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,50 +1,69 @@
 <template>
-    <div id="about">
-        <h1>About</h1>
-        <BlockQuoteContent
-            content="Every person has two education, one whihc he receive from other, and one, more important, which he gives himself."
-            author="Edward Gibbon"
-            occupation="British historian"
-        ></BlockQuoteContent>
-        <div class="flex-container">
-            <img class="profile" :src="profileImage" alt="profileImage">
-            <p>{{profileDescription}}</p>
-        </div>
+  <div id="about">
+    <h1>About</h1>
+    <BlockQuoteContent
+      content="Every person has two education, one whihc he receive from other, and one, more important, which he gives himself."
+      author="Edward Gibbon"
+      occupation="British historian"
+    ></BlockQuoteContent>
+    <div class="flex-container">
+      <img
+        class="profile"
+        :src="profileImage"
+        alt="profileImage"
+      >
+      <p>{{profileDescription}}</p>
     </div>
+
+    <!-- Prepare a container for your calendar. -->
+    <div class="calendar">
+      <!-- Loading stuff -->
+      Loading the data just for you.
+    </div>
+  </div>
 </template>
 
 <script>
 import BlockQuoteContent from '../components/BlockQuoteContent.vue';
+import GitHubCalendar from "github-calendar";
+
 export default {
-    components: { BlockQuoteContent },
-    data: () => ({
-        profileImage: require('@/assets/profile/profile.jpg'),
-        profileDescription: `Shijie Zhou studies at University of California, San Diego in the Interdisciplinary Computer in Arts. He is also the member for design and construct web developer at the Worldview project organization. Currently, he is working in Akirix(MMSG department) with software engineer.
+  components: { BlockQuoteContent },
+  data: () => ({
+    profileImage: require('@/assets/profile/profile.jpg'),
+    profileDescription: `Shijie Zhou studies at University of California, San Diego in the Interdisciplinary Computer in Arts. He is also the member for design and construct web developer at the Worldview project organization. Currently, he is working in Akirix(MMSG department) with software engineer.
 
 He used to work on programming artist and digital image designer at the Digital Gym Cinema as internet. He obtains Associate degree in Green River College, and also finish the B.A. degree at UCSD.
 He gained experience in media use and computer art. He also familiar with web design and customize selling pages on ebay which promote stuffs at leisure time..`
-    })
+  }),
+  mounted() {
+    GitHubCalendar(".calendar", "shijiezhou1");
+    // or enable responsive functionality
+    GitHubCalendar(".calendar", "shijiezhou1", { responsive: true });
+  }
 };
 </script>
 
-<style lang="scss" scoped="scoped">
+<style lang="scss">
 #about {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    //TODO fix scoped later
+  @import '@/assets/styles/github-calendar-responsive.scss';
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 
-    & img {
-        max-width: 100px;
-    }
-    
-    .profile {
-        float: right;
-    }
-    
-    .flex-container {
-        overflow: auto;
-    }
+  & img {
+    max-width: 100px;
+  }
+
+  .profile {
+    float: right;
+  }
+
+  .flex-container {
+    overflow: auto;
+  }
 }
 </style>
