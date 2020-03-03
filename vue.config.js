c1610ea3123a3a43dd748c11680ba2a1ce581609
Shijie Zhou <cs5129606@gmail.com>
2020-03-03 17:07:37 +0800

    Fixed Config File

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     filenameHashing: true,
     publicPath: process.env.NODE_ENV === 'production' ?
         '/':
-        '/vue-app/'
+        '/vue-app/',
     lintOnSave: process.env.NODE_ENV !== 'production',
     pages: {
         index: {
