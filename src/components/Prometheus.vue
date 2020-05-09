08412eea356c9a0c79f360c8c88b78bc3a6d65d8
Shijie Zhou <cs5129606@gmail.com>
2020-05-09 22:46:06 +0800

    Redirect

--- src/components/Prometheus.vue
+++ src/components/Prometheus.vue
@@ -9,6 +9,7 @@
         <a
           href="#"
           class="info-link"
+          v-on:click="viewProject(item.path)"    
         >
           <img
             class="section-img profile"
@@ -35,16 +36,21 @@ export default {
   },
   mounted() {
     this.items = [
-      { img: require('@/assets/images/9images/1.jpg'), msg: 'Bristlebot', des: 'Mimicking a reptile.', },
-      { img: require('@/assets/images/9images/2.jpg'), msg: 'Telesignalization', des: 'Telesignalization is a human relationship meansure', },
-      { img: require('@/assets/images/9images/3.jpg'), msg: 'Music Player', des: 'New way to play music on your webpage.', },
-      { img: require('@/assets/images/9images/4.jpg'), msg: 'Paper Cage', des: 'A furniture that print by 3D material.', },
-      { img: require('@/assets/images/9images/5.jpg'), msg: 'Personal Information', des: 'Learn about me', },
-      { img: require('@/assets/images/9images/6.jpg'), msg: 'Coconut Factory', des: 'A game design to have fun', },
-      { img: require('@/assets/images/9images/7.jpg'), msg: 'Zoe Trope', des: 'A zoetrope is one of several pre-film animation devices that produce the illusion of motion', },
-      { img: require('@/assets/images/9images/8.jpg'), msg: 'Escape City', des: 'Unity Game that make by personal drawing', },
-      { img: require('@/assets/images/9images/9.jpg'), msg: 'Practical Cup', des: 'A 3D cup that makes by meshmixer', },
+      { path: "about", img: require('@/assets/images/9images/1.jpg'), msg: 'Bristlebot', des: 'Mimicking a reptile.', },
+      { path: "about", img: require('@/assets/images/9images/2.jpg'), msg: 'Telesignalization', des: 'Telesignalization is a human relationship meansure', },
+      { path: "about", img: require('@/assets/images/9images/3.jpg'), msg: 'Music Player', des: 'New way to play music on your webpage.', },
+      { path: "about", img: require('@/assets/images/9images/4.jpg'), msg: 'Paper Cage', des: 'A furniture that print by 3D material.', },
+      { path: "about", img: require('@/assets/images/9images/5.jpg'), msg: 'Personal Information', des: 'Learn about me', },
+      { path: "about", img: require('@/assets/images/9images/6.jpg'), msg: 'Coconut Factory', des: 'A game design to have fun', },
+      { path: "about", img: require('@/assets/images/9images/7.jpg'), msg: 'Zoe Trope', des: 'A zoetrope is one of several pre-film animation devices that produce the illusion of motion', },
+      { path: "about", img: require('@/assets/images/9images/8.jpg'), msg: 'Escape City', des: 'Unity Game that make by personal drawing', },
+      { path: "about", img: require('@/assets/images/9images/9.jpg'), msg: 'Practical Cup', des: 'A 3D cup that makes by meshmixer', },
     ]
+  },
+   methods: {
+    viewProject: function (path) {
+        this.$router.push({ path })
+    }
   }
 }
 </script>
