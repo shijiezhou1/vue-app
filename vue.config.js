bb8d513a48f1b5fa4754192b0710795add791c0f
Shijie Zhou <cs5129606@gmail.com>
2020-06-11 00:16:46 +0800

    update package json

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 
 const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   filenameHashing: true,
@@ -22,16 +21,27 @@ module.exports = {
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
-    }
+    },
   },
+
+  crossorigin: undefined,
+
   devServer: {
     disableHostCheck: true,
     port: 8080,
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/',
+        ws: false,
+        changeOrigin: true
+      }
+    }
   },
   configureWebpack: {
     resolve: {
       alias: {
+        'vue$': true ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
         "@": path.resolve(__dirname, "src"),
       },
     },
