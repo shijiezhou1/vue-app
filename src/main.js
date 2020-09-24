ad79b1ecdf6655d476aa635bd0fe20ad3e506e3b
Shijie Zhou <cs5129606@gmail.com>
2020-09-24 08:18:53 +0800

    Update socket io

--- src/main.js
+++ src/main.js
@@ -77,7 +77,7 @@ Vue.toasted.register('appInfo',
 // Now setup our socket and vuex configuration
 Vue.use(new VueSocketIO({
   debug: true,
-  connection: process.env.VUE_APP_API, // 'http://localhost:8000', //TODO setup environment variable.
+  connection: process.env.VUE_APP_SOCKET_API, // 'http://localhost:8000', //TODO setup environment variable.
   vuex: {
     store,
     actionPrefix: 'SOCKET_'
